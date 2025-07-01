e733886f72f507c9812551b22354aebc446a1bc8
MatsRivel <mats-r@hotmail.com>
2025-07-01 15:44:06 +0200

    123

--- src/entities/player/mod.rs
+++ src/entities/player/mod.rs
@@ -1,7 +1,7 @@
-use crate::movement::{acceleration::{angular_instant::InstantAngularAcceleration, instant::InstantAcceleration}, velocity::{angular_maintained::MaintainedAngularVelocity, instant::InstantVelocity, maintained::MaintainedVelocity}};
+use crate::movement::{acceleration::{angular_instant::InstantAngularAcceleration, instant::InstantAcceleration}, velocity::{angular_instant::InstantAngularVelocity, angular_maintained::MaintainedAngularVelocity, application::{apply_instant_angular_velocity, apply_instant_velocity}, instant::InstantVelocity, maintained::MaintainedVelocity}};
 use super::*;
 #[derive(Component, Default)]
-#[require(Object, InstantVelocity)]
+#[require(Object)]
 pub struct PlayerTag;
 
 // Used when velocity is reset every turn. There is no acceleration, only instantaneous velocity.
@@ -63,7 +63,42 @@ pub fn apply_acceleration_to_single_from_keyboard<T:Component>(
     let rotation_adjusted_movement = transform.rotation.mul_vec3(momentum.extend(0.0)).truncate();
     let delta_time_movement = rotation_adjusted_movement*time.delta_secs();
     *acceleration += delta_time_movement;
+    
 
+}
+
+pub fn apply_acceleration_rotation_velocity_to_single_from_keyboard<T:Component>(
+    time: Res<Time>,
+    keyboard_input: Res<ButtonInput<KeyCode>>,
+    query: Single<&mut InstantAngularAcceleration, With<T>>,
+){
+    let mut angular_acceleration = query.into_inner();
+    let clockwise = keyboard_input.pressed(KeyCode::KeyD);
+    let counterwise = keyboard_input.pressed(KeyCode::KeyA);
+    let rotary_momentum = match (counterwise, clockwise){
+        (true,false) => 1.0,
+        (false,true) => -1.0,
+        (true,true) | (false,false) => 0.0
+    };
+    
+    *angular_acceleration += rotary_momentum * time.delta_secs();
+}
+
+pub fn apply_angular_velocity_to_single_from_keyboard<T:Component>(
+    time: Res<Time>,
+    keyboard_input: Res<ButtonInput<KeyCode>>,
+    query: Single<&mut InstantAngularVelocity, With<T>>,
+){
+    let mut angular_velocity = query.into_inner();
+    let clockwise = keyboard_input.pressed(KeyCode::KeyD);
+    let counterwise = keyboard_input.pressed(KeyCode::KeyA);
+    let rotary_momentum = match (counterwise, clockwise){
+        (true,false) => 1.0,
+        (false,true) => -1.0,
+        (true,true) | (false,false) => 0.0
+    };
+    
+    *angular_velocity += rotary_momentum * time.delta_secs();
 }
 
 #[derive(Default)]
@@ -72,21 +107,37 @@ impl Plugin for PlayerPlugin {
     fn build(&self, app: &mut App) {
         app.add_systems(Startup,    (
             spawn_player, 
-        )).add_systems(Update, apply_velocity_to_single_from_keyboard::<PlayerTag>);
+        )).add_systems(FixedUpdate, (
+            apply_instant_velocity,
+            apply_instant_angular_velocity))
+        .add_systems(Update, (
+            apply_velocity_to_single_from_keyboard::<PlayerTag>,
+            apply_angular_velocity_to_single_from_keyboard::<PlayerTag>,
+        ));
     }
 }
 pub fn spawn_player(mut commands: Commands, asset_server: Res<AssetServer>){
-    let asset_path = r"sprites\Ships\ship-a\ship-a1.png";
+    let asset_path = r"sprites\Ships\ship-a\ship-a2.png";
     let image = asset_server.load(asset_path);
     let mut sprite =     Sprite::from_image(image);
     sprite.custom_size = Some(Vec2::splat(128.0));
     let player_entity = commands.spawn((
         PlayerTag,
         sprite,
-        MaintainedVelocity::default(),
-        MaintainedAngularVelocity::default(),
-        InstantAcceleration::default(),
-        InstantAngularAcceleration::default(),
+        // InstantVelocity::default(),
+        InstantAngularVelocity::default(),
+        // InstantAcceleration::default(),
+        // InstantAngularAcceleration::default(),
+    )).id();
+
+    let asset_path = r"AI_Assets\Body\Space_sattelite_core...-1670999352-0 (1).png";
+    let image = asset_server.load(asset_path);
+    let mut sprite =     Sprite::from_image(image);
+    sprite.custom_size = Some(Vec2::splat(128.0));
+    let player_entity = commands.spawn((
+        Transform::from_translation(Vec3::new(50.0, 50.0, 0.0)),
+        sprite,
+
     )).id();
     println!("Player Entity: {player_entity:?}");
 }
--- src/movement/velocity/application.rs
+++ src/movement/velocity/application.rs
@@ -1,4 +1,5 @@
 use super::*;
+use crate::movement::velocity::angular_instant::InstantAngularVelocity;
 use crate::movement::velocity::instant::InstantVelocity;
 use crate::movement::velocity::maintained::MaintainedVelocity;
 
@@ -12,5 +13,16 @@ pub fn apply_instant_velocity(mut query: Query<(&mut Transform, &mut InstantVelo
 pub fn apply_maintained_velocity(mut query: Query<(&mut Transform, &mut MaintainedVelocity)>){
     for (mut transform, mut velocity) in query.iter_mut(){
         *transform += &mut *velocity;
+    }
+}
+
+pub fn apply_instant_angular_velocity(
+    time: Res<Time>,
+    mut query: Query<(&mut Transform, &mut InstantAngularVelocity)>,
+){
+    for (mut transform, mut angular_velocity) in query.iter_mut() {
+        let delta_angle = angular_velocity.consume() * time.delta_secs();
+        let delta_rotation = Quat::from_rotation_z(delta_angle);
+        transform.rotation *= delta_rotation;
     }
 }
